c3ba04c632141f7e33264c0b1126076861036d1a
Purva Bansal <purva.bansal@gmail.com>
2016-05-12 03:48:19 +0530

    modified script for calculating cosine similarity

--- twitter_data.py
+++ twitter_data.py
@@ -5,44 +5,72 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy
 import mmap
+from nltk.corpus import stopwords
 
-negative_words_file=open('negative-words.txt')
-negative_words = mmap.mmap(negative_words_file.fileno(), 0, access=mmap.ACCESS_READ)
-positive_words_file=open('positive-words.txt')
-positive_wordss = mmap.mmap(positive_words_file.fileno(), 0, access=mmap.ACCESS_READ)
-consumer_key = 'APe74j0gQL47Qsp0xHSmxGsqQ'
-consumer_secret = 'SlRZIDYEbSPIHO28qwFS2rKCwdQgnkiImHvsEzCvFjdBmtQfL1'
-access_token = '269631612-nOz98VEBW8WWCLiVVvPd5EHBzXwJdXHzNNGHuHQi'
-access_secret = 'TjwifaCX8jI2RRcdD0zmmYrYfQx9sOREld98ezAeEv0TK'
- 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth)
+try:
+	negative_words = [line.strip() for line in open("negative-words.txt", 'r')]
+	positive_words = [line.strip() for line in open("positive-words.txt", 'r')]
 
-list_tweets = []
-list_tweets_emotion = []
-for mytweets in tweepy.Cursor(api.search, q='#TerrorismHasNoReligion', lang='en').items(100):
-	tweet_data = unicode(mytweets.text).encode('utf-8')
-	list_tweets.append(tweet_data)
-	temp_emotion_tweet = []
-	for word in tweet_data.split():
-		# if word in negative_words or word in positive_words:
-		if negative_words.find(word) != '-1' or positive_words.find(word) != '-1':
-			temp_emotion_tweet.append(word)
-	list_tweets_emotion.append(' '.join(temp_emotion_tweet))
+	# consumer_key = 'APe74j0gQL47Qsp0xHSmxGsqQ'
+	# consumer_secret = 'SlRZIDYEbSPIHO28qwFS2rKCwdQgnkiImHvsEzCvFjdBmtQfL1'
+	# access_token = '269631612-nOz98VEBW8WWCLiVVvPd5EHBzXwJdXHzNNGHuHQi'
+	# access_secret = 'TjwifaCX8jI2RRcdD0zmmYrYfQx9sOREld98ezAeEv0TK'
+	 
+	# auth = OAuthHandler(consumer_key, consumer_secret)
+	# auth.set_access_token(access_token, access_secret)
+	 
+	# api = tweepy.API(auth)
+
+	original_tweets = open("tweets.txt", 'r')
+
+	list_tweets_abstraction = []
+	list_tweets_emotion = []
+	list_tweets = []
+	stopwords = stopwords.words('english')
+	for i,term in enumerate(stopwords):
+		stopwords[i] = term.encode('utf-8')
 
 
-# print list_tweets
+	# for mytweets in tweepy.Cursor(api.search, q='#TerrorismHasNoReligion', lang='en').items(5):
+	for mytweets in original_tweets:
+		# tweet_data = unicode(mytweets.text).encode('utf-8')
+		tweet_data = mytweets.lower()
+		list_tweets.append(tweet_data)
+		tweet_data = tweet_data.replace('#TerrorismHasNoReligion'.lower(),'')
+		temp_emotion_tweet = []
+		temp_abstract_tweet = []
+		splitted_tweet = tweet_data.split()
+		for word in splitted_tweet:
+			# if word in negative_words or word in positive_words:
+			if word in negative_words != -1 or word in positive_words != -1:
+				temp_emotion_tweet.append(word)
+			else:
+				if word not in stopwords and len(word) >= 2:
+					temp_abstract_tweet.append(word)
+		list_tweets_abstraction.append(' '.join(temp_abstract_tweet))
+		list_tweets_emotion.append(' '.join(temp_emotion_tweet))
 
-tfidf_vectorizer = TfidfVectorizer()
+	print "---------list tweets-------------"
+	print list_tweets_abstraction
 
-tfidf_matrix_abstraction = tfidf_vectorizer.fit_transform(list_tweets)
-matrix_cosine_abstraction = cosine_similarity(tfidf_matrix_abstraction, tfidf_matrix_abstraction)
+	print "---------list emotion tweets--------------"
+	print list_tweets_emotion
+	tfidf_vectorizer = TfidfVectorizer()
 
+	tfidf_matrix_abstraction = tfidf_vectorizer.fit_transform(list_tweets_abstraction)
+	matrix_cosine_abstraction = cosine_similarity(tfidf_matrix_abstraction, tfidf_matrix_abstraction)
+	numpy.savetxt("cosine_abstraction.csv", matrix_cosine_abstraction, delimiter=",")
 
-tfidf_matrix_emotion = tfidf_vectorizer.fit_transform(list_tweets_emotion)
-matrix_cosine_emotion = cosine_similarity(tfidf_matrix_emotion, tfidf_matrix_emotion)
+	tfidf_matrix_emotion = tfidf_vectorizer.fit_transform(list_tweets_emotion)
+	matrix_cosine_emotion = cosine_similarity(tfidf_matrix_emotion, tfidf_matrix_emotion)
+	numpy.savetxt("cosine_emotion.csv", matrix_cosine_emotion, delimiter=",")
 
-# print tfidf_matrix_abstraction
-# print tfidf_matrix_emotion
+	list_tweets_file = open('list_tweets', 'w')
+	list_tweets_file.write("\n".join(list_tweets))
+	print "---------matrix_cosine_emotion---------------"
+	print matrix_cosine_emotion
+
+	print "---------matrix_cosine_abstraction---------------"
+	print matrix_cosine_abstraction
+except IOError:
+	print "some error"
